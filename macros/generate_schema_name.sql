467443a686209699a16e5b9d111fe9076455759c
Devashish Nalapareddy <110732820+devashish4785@users.noreply.github.com>
2026-08-10 23:52:25 -0500

    Fix typo in comment for clarity

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,7 +2,7 @@
     {% if custom_schema_name %}
         {{ custom_schema_name }}
     {% else %}
-    -- adding commment for understnading git ub
+    -- adding commment for understanding github
         {{ target.schema }}
     {% endif %}
 {% endmacro %}
