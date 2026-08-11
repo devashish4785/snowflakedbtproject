0eefbe09e01e73bdd240a681a0c59a86b5f7d293
Devashish Nalapareddy <110732820+devashish4785@users.noreply.github.com>
2026-08-10 23:51:56 -0500

    Add comment to generate_schema_name macro
    Added a comment for better understanding of the code.

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,6 +2,7 @@
     {% if custom_schema_name %}
         {{ custom_schema_name }}
     {% else %}
+    -- adding commment for understnading git ub
         {{ target.schema }}
     {% endif %}
 {% endmacro %}
